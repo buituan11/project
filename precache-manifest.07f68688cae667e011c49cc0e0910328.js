55359b774b3ca5633f9b5114b1cb46337393b2b0
buituan11 <buituan71099@gmail.com>
2019-07-17 01:07:29 +0700

    Updates

---
+++ precache-manifest.07f68688cae667e011c49cc0e0910328.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ee804d0533582deb2a970777461902",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "28dd35222ebce68e5c65",
+    "url": "/project/static/css/main.bcc9aaa0.chunk.css"
+  },
+  {
+    "revision": "cf9fba91b7276e119953",
+    "url": "/project/static/js/2.21767ef2.chunk.js"
+  },
+  {
+    "revision": "28dd35222ebce68e5c65",
+    "url": "/project/static/js/main.0aff46a7.chunk.js"
+  },
+  {
+    "revision": "369bc2d987da7beb9860",
+    "url": "/project/static/js/runtime~main.4b9e7fa5.js"
+  }
+]);
